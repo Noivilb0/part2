a662a89573b99a8fa915f910f06cb1bf4cd8c674
Noibilbo <mohamedcod2@gmail.com>
2025-01-04 05:44:28 +0100

    integrated_in_one_node

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,53 +1,108 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include <iostream>
 #include <thread>
-#include <chrono>
+#include <atomic>
 
-class UINode : public rclcpp::Node {
+class RobotControlNode : public rclcpp::Node {
 public:
-    UINode() : Node("ui_node") {
-        pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    RobotControlNode()
+        : Node("robot_control_node"),
+          linear_velocity_(0.0),
+          angular_velocity_(0.0),
+          in_bounds_(true) {
+        // Subscriber for odometry
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/odom", 10, std::bind(&RobotControlNode::odomCallback, this, std::placeholders::_1));
+
+        // Publisher for velocity commands
+        cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+
+        // Boundary limits
+        x_min_ = 1.0;
+        x_max_ = 9.0;
+        y_min_ = 1.0;
+        y_max_ = 9.0;
     }
 
-    void sendCommand(double linear, double angular) {
-        geometry_msgs::msg::Twist cmd;
-        cmd.linear.x = linear;
-        cmd.angular.z = angular;
+    void run() {
+        std::thread input_thread([this]() { getUserInput(); });
 
-        // Publish command for 1 second
-        for (int i = 0; i < 10; ++i) {
-            pub_->publish(cmd);
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        rclcpp::Rate rate(10); // 10 Hz
+        while (rclcpp::ok()) {
+            if (in_bounds_) {
+                publishCommand();
+            } else {
+                stopRobot();
+            }
+            rclcpp::spin_some(this->get_node_base_interface());
+            rate.sleep();
         }
 
-        // Stop the robot
-        cmd.linear.x = 0.0;
-        cmd.angular.z = 0.0;
-        pub_->publish(cmd);
+        input_thread.join();
     }
 
 private:
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
-};
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
 
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv);
+    double x_min_, x_max_, y_min_, y_max_;
+    std::atomic<double> linear_velocity_;
+    std::atomic<double> angular_velocity_;
+    nav_msgs::msg::Odometry robot_odom_;
+    std::atomic<bool> in_bounds_;
 
-    auto node = std::make_shared<UINode>();
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+        robot_odom_ = *msg;
+        double x = robot_odom_.pose.pose.position.x;
+        double y = robot_odom_.pose.pose.position.y;
 
-    while (rclcpp::ok()) {
-        double linear, angular;
-        std::cout << "Enter linear velocity: ";
-        std::cin >> linear;
-        std::cout << "Enter angular velocity: ";
-        std::cin >> angular;
-
-        node->sendCommand(linear, angular);
+        // Check boundaries
+        if (x < x_min_ || x > x_max_ || y < y_min_ || y > y_max_) {
+            in_bounds_ = false;
+        } else {
+            in_bounds_ = true;
+        }
     }
 
+    void publishCommand() {
+        geometry_msgs::msg::Twist cmd;
+        cmd.linear.x = linear_velocity_;
+        cmd.angular.z = angular_velocity_;
+        cmd_pub_->publish(cmd);
+    }
+
+    void stopRobot() {
+        geometry_msgs::msg::Twist stop_msg;
+        stop_msg.linear.x = 0.0;
+        stop_msg.angular.z = 0.0;
+        cmd_pub_->publish(stop_msg);
+
+        RCLCPP_WARN_ONCE(this->get_logger(), "Robot out of bounds! Stopping.");
+    }
+
+    void getUserInput() {
+        while (rclcpp::ok()) {
+            double linear, angular;
+            std::cout << "Enter linear velocity (m/s): ";
+            std::cin >> linear;
+            std::cout << "Enter angular velocity (rad/s): ";
+            std::cin >> angular;
+
+            linear_velocity_ = linear;
+            angular_velocity_ = angular;
+
+            std::cout << "Commands will be published until boundary conditions are met.\n";
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<RobotControlNode>();
+    node->run();
     rclcpp::shutdown();
     return 0;
 }
-
 
